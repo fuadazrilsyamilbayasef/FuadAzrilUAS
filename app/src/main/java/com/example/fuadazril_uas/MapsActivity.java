4b2beb448b4dfb91d2510f3281ac2874b66dfa21
unknown <fuadazril0101012@gmailcom>
2022-07-07 20:49:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fuadazril_uas/MapsActivity.java
@@ -0,0 +1,42 @@
+package com.example.fuadazril_uas;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.fuadazril_uas.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng UNPAM_Viktor = new LatLng(-6.3459975, 106.6894696);
+        mMap.addMarker(new MarkerOptions().position(UNPAM_Viktor).title("UNPAM_Viktor"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UNPAM_Viktor));
+    }
+}
